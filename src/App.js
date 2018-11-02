9e013e066c987761b07ebb7471449e4a268bee2b
Vasiliy Kolesnikov <v.a.kolesnikov@bk.ru>
2018-11-03 01:52:11 +0300

    bootstrap

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 class App extends Component {
     state = {
         votes: 0,
+        step: 0,
         candidates: new Array(10).fill(0),
     };
 
@@ -11,6 +12,8 @@ class App extends Component {
         const count = 10;
         const candidates = new Array(count).fill(0);
         let votes = 0;
+
+        this.clearState();
 
         const generateVote = () => {
             const id = Math.floor(Math.random() * candidates.length);
@@ -25,49 +28,81 @@ class App extends Component {
             10
         );
 
-        const udateState = setInterval(
+        const updateState = setInterval(
             () => {
                 this.setState({
                     votes,
                     candidates,
+                    step: ++this.state.step,
                 })
             },
             1000
         );
 
         setTimeout(
-            () => clearInterval(generateVoteId),
+            () => {
+                clearInterval(generateVoteId);
+                clearInterval(updateState);
+            },
             9000
         );
     };
 
+    clearState = () => this.setState({
+       votes: 0,
+       step: 0,
+       candidates: new Array(10).fill(0),
+    });
+
     renderCandidates = () => {
       const { votes, candidates} = this.state;
         return candidates.map((item, index) => (
-          <div>
-              Кандидат №{index + 1}: { item } голосов ({ ((item/votes) * 100).toFixed(1) })%
-          </div>
+          <tr>
+              <td>{ index + 1 }</td>
+              <td>Кандидат { index + 1 }</td>
+              <td>{item}</td>
+              <td>{ ((item/votes) * 100).toFixed(1) || '-' }</td>
+          </tr>
         ));
     };
+
+    renderTableHeader = () => (
+        <thead className="thead-dark">
+        <tr>
+            <th> # </th>
+            <th> Кандидат </th>
+            <th> Количество голосов </th>
+            <th> % голосов </th>
+        </tr>
+        </thead>
+    );
 
 
 
 
     render() {
-
-
         return (
-          <div className="App">
+          <div className="app">
               <div>
-                  { this.renderCandidates() }
+                  <table className='table'>
+                      { this.renderTableHeader() }
+                      <tbody className='table-striped'>
+                          { this.renderCandidates() }
+                      </tbody>
+                  </table>
               </div>
-              <div>
-                  Всего голосов: { this.state.votes }
+              <div className='d-flex justify-content-between'>
+                  <button
+                      type="button"
+                      className="btn btn-secondary btn-lg bg-dark"
+                      onClick={ this.updateState }
+                  >
+                      Начать голосование
+                  </button>
+                  <div>
+                      До конца голосования осталось { 9 - this.state.step } сек.
+                  </div>
               </div>
-
-              <button onClick={ this.updateState }>
-                  Начать голосование
-              </button>
           </div>
         );
 }
